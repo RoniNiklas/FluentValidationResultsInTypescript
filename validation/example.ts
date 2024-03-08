7fe154decc51c3f3c96c1bee9095fd7a615f2494
Roni Uusoksa <Roni.Uusoksa@syke.fi>
2024-03-08 13:25:51 +0200

    add examples as comments as not everyone has quokka

--- validation/example.ts
+++ validation/example.ts
@@ -19,13 +19,13 @@ const resultRaw = JSON.parse(failureMessage)
 const resultTyped = resultRaw as FluentValidationError<CreateOrUpdateShippingDocument>
 
 // Keys that exist in NestedKeys of type T return the error when it exists
-console.log(getBackendErrorMessage(resultTyped, 'WasteHolderHouseHold'))
-console.log(getBackendErrorMessage(resultTyped, 'WasteHolderCompany.BusinessId'))
-console.log(getBackendErrorMessage(resultTyped, 'Destination.Location.Latitude'))
-console.log(getBackendErrorMessage(resultTyped, 'Wastes[0].RecoveryCode'))
+console.log(getBackendErrorMessage(resultTyped, 'WasteHolderHouseHold')) // ["Household must be null/empty when Wasteholder company is sent."]
+console.log(getBackendErrorMessage(resultTyped, 'WasteHolderCompany.BusinessId')) // ["A company has to either have a Finnish business identifier (Y-tunnus), a foreign business identifier or be an organization without a business identifier (e.g. hoitokunta, osakaskunta)"]
+console.log(getBackendErrorMessage(resultTyped, 'Destination.Location.Latitude')) // ["This value is not within Finland's borders."],
+console.log(getBackendErrorMessage(resultTyped, 'Wastes[0].RecoveryCode')) // ["Waste has to end up in a laboratory analysis (ShippedToLaboratoryForAnalysis), or have a recovery code or a disposal code. It can not have more than one of these three."]
 
 // Keys that exist in NestedKeys of type T return undefined when there is no validation error
-console.log(getBackendErrorMessage(resultTyped, 'WasteHolderContact.Email'))
+console.log(getBackendErrorMessage(resultTyped, 'WasteHolderContact.Email')) // undefined
 
 // Keys that do not exist in NestedKeys of type T are not allowed and will throw a compile time error
 console.log(getBackendErrorMessage(resultTyped, 'WasteHolderContact.KeyDoesntExist'))
